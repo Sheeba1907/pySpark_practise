14491e2605895dc8837046445eed7a8cf93f6248
Sheeba1907 <sheeba.banu@diggibyte.com>
2025-10-07 18:31:47 +0530

    This is practise for window, array functions

--- src/file5.py
+++ src/file5.py
@@ -5,7 +5,9 @@ os.environ['PYSPARK_DRIVER_PYTHON'] = r'C:\Users\sheeb\AppData\Local\Programs\Py
 
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import *
+from pyspark.sql.functions import udf
 from pyspark.sql.window import Window
+from pyspark.sql.types import *
 
 spark = SparkSession.builder\
         .appName("Spark func1")\
@@ -78,3 +80,45 @@ dff = dff.withColumn("Distinct array", array_distinct(col("scores")))
 dff = dff.withColumn("Sorted array", sort_array(col("scores")))
 dff = dff.withColumn("Transformation", transform(col("scores"), lambda x: x + 10))
 dff.show(truncate = False)
+
+#UDF
+datas = [("Alice", 25), ("Bob", 30), ("Charlie", 35)]
+dfs = spark.createDataFrame(datas, ["name", "age"])
+dfs.show(truncate=False)
+
+def age_category(age):
+  if age < 18:
+    return "Minor"
+  elif 18 <= age <=30:
+    return "Young"
+  else:
+    return "Adult"
+age_udf = udf(age_category, StringType())
+rdfs = dfs.withColumn("Category", age_udf(col("age")))
+rdfs.show(truncate=False)
+
+dats = [(25000,), (60000,), (120000,), (45000,)]
+dfff = spark.createDataFrame(dats, ["salary"])
+
+def salary_category(salary):
+  if salary < 20000:
+    return "Low"
+  elif 20000 <= salary < 50000:
+    return "Normal"
+  else:
+    return "High Payable"
+salary_udf = udf(salary_category, StringType())
+rdfff = dfff.withColumn("Category", salary_udf(col("salary")))
+rdfff.show()
+
+data1 = [("23",), ("45",), ("56",), ("78",), ("96",), ("12",), ("33",), ("77",)]
+df1 = spark.createDataFrame(data1, ["numbers"])
+def even_or_odd(numbers):
+  n = int(numbers)
+  if n %2 == 0:
+      return  "even"
+  else:
+      return "odd"
+num_udf = udf(even_or_odd, StringType())
+rdf1 = df1.withColumn("Even or Odd", num_udf(col("numbers")))
+rdf1.show()
